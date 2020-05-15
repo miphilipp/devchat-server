a29b457c46eae0f2530efe6644e5820bc466056d
Philipp Michaelis <philipp.michaelis@me.com>
2020-05-15 20:09:59 +0200

    Repariert Session-Test

--- internal/communication/session/session_test.go
+++ internal/communication/session/session_test.go
@@ -2,37 +2,37 @@ package session
 
 import (
 	"testing"
+
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
 func TestVerifyToken(t *testing.T) {
-	const expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicGhpbGlwcCIsImV4cCI6MzMzMzN9.d8btnRKRO7dsv4pg12KC-ysWicVVXfd3pEw47Gl8orY"
-	const validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicGhpbGlwcCIsImV4cCI6NjU4NDM3MjQ4Mn0.IfGHeFRFkpF_N5vF_fPSI6epuQJ1xCoKA14GU1imE94"
+	secret := []byte("l36FjiSFBgXxyakipXUtxrs2V7wtGzHX")
+	const expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJleHAiOjEwMTYyMzkwMjJ9.h7WNmCGVtXwQxH1OJ4NoZRWd7ZqImlRNf_Ec_CI9dAg"
+	const validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJleHAiOjMwMTYyMzkwMjJ9.-dtxuB6fCXk58w_EpceYLy6aYJAdwyVg0KWl4DDJPQc"
 	const badToken = "eyJhbGciOiJIUzddfI1NiJ9.eyJuYW1lIjoicGhpbGlwcCIsImV4cCIff4?6NjU4NDM3MjQ4Mn0.dK9QlWp6hr-eDObbRq53lCGQ1dwUaES_5zb2a_AOuOg"
 
-	claims, err := verifyToken(expiredToken)
+	claims, err := VerifyToken(expiredToken, secret)
 	if err == nil {
 		t.Fail()
-	} else {
-		t.Log(err.Error())
 	}
 
 	name := claims.(jwt.MapClaims)["name"].(string)
-	if name != "philipp" {
+	if name != "John Doe" {
 		t.Fail()
 	}
 
-	claims, err = verifyToken(validToken)
+	claims, err = VerifyToken(validToken, secret)
 	if err != nil {
-		t.Fail()
+		t.Error(err.Error())
 	}
 
 	name = claims.(jwt.MapClaims)["name"].(string)
-	if name != "philipp" {
+	if name != "John Doe" {
 		t.Fail()
 	}
 
-	claims, err = verifyToken(badToken)
+	claims, err = VerifyToken(badToken, secret)
 	if err == nil || claims != nil {
 		t.Fail()
 	}
